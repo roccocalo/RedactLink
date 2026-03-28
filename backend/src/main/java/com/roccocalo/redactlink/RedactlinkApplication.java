2610c6c8b98e34b38b580c9302c3bdb19848daa2
Rocco Caló <roccocalo03@gmail.com>
2026-03-28 18:25:15 +0100

    initialized project

---
+++ backend/src/main/java/com/roccocalo/redactlink/RedactlinkApplication.java
@@ -0,0 +1,13 @@
+package com.roccocalo.redactlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedactlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedactlinkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/roccocalo/redactlink/RedactlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roccocalo.redactlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedactlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
